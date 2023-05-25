7f24eb7f389748f824a59853322c367aa6984322
suraj7038326999 <133981553+suraj7038326999@users.noreply.github.com>
2023-05-25 12:14:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,37 +14,18 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
- 
-
 # Let's put a pick list here so they can pick the fruit they want to include 
-
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
- 
-
 # Display the table on the page.
-
 streamlit.dataframe(fruits_to_show)
-
- 
-
 # create the repeatable code block (called a function)
-
 def get_fruityvice_data(this_fruit_choice):
-
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-    return fruityvice_normalized
-
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+return fruityvice_normalized
 # New Section to display fruityvice api response
-
 streamlit.header("Fruityvice Fruit Advice!")
-
 try:
 
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
